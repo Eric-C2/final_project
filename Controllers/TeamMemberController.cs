8f1ff373d610cdc966d70c47b95286c3fb466be5
eric coomer <ericcoomer1@gmail.com>
2025-08-04 16:04:02 -0400

    TeamMember table CRUD ops / Controller stuff / Interface

--- Controllers/TeamMemberController.cs
+++ Controllers/TeamMemberController.cs
@@ -1,22 +1,91 @@
 using Microsoft.AspNetCore.Mvc;
 using final_project.Models;
+using final_project.Data;
+using final_project.Controllers;
+using System.Runtime.InteropServices;
 
 namespace final_project.Controllers
 {
     [ApiController]
-    [Route("[controller]")] 
+    [Route("[controller]")]
     public class TeamMemberController : ControllerBase
-    {
-        private readonly ILogger<TeamMemberController> _logger;
 
-        public TeamMemberController(ILogger<TeamMemberController> logger)
+    {
+
+        public required TeamMemberContextDAO _context;
+
+        public TeamMemberController(TeamMemberContextDAO context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult GetById(int id)
         {
+            var result = _context.GetTeamMemberById(id);
+
+            if (result == null)
+                return NotFound(id);
+
+            return Ok(result);
+   
+        }
+
+        [HttpPost]
+        public IActionResult Post(TeamMember member)
+        {
+
+            var result = _context.AddTeamMember(member);
+
+            if (result == null)
+                return StatusCode(500, "Member already exists");
+
+
+            if (result == 0)
+                return StatusCode(500, "An error occurred while processing your request.");
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var result = _context.RemoveTeamMemberById(id);
+
+            if (result == null)
+                return NotFound();
+
+            if (result == 0)
+                return StatusCode(500, "An error occured while processing your request.");
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put(TeamMember member) 
+        {
+            var result = _context.UpdateMember(member);
+
+            if (result == null)
+                return NotFound(member.Id);
+
+            if (result == 0)
+                return StatusCode(500, "An error occured while processing your request");
+
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult UpdateTeamMember(TeamMember member) 
+        {
+            var result = _context.UpdateMember(member);
+
+            if (result == null)
+                return NotFound(member.Id);
+
+            if (result == 0)
+                return StatusCode(500, "An error occured while processing your request");
+
             return Ok();
         }
     }
