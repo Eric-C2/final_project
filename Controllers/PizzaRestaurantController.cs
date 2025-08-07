2249daba876127a09a6bb9b3861557da4a2357d6
RivalJ <157822657+RivalJ@users.noreply.github.com>
2025-08-07 19:35:07 -0400

    added DAO

--- Controllers/PizzaRestaurantController.cs
+++ Controllers/PizzaRestaurantController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using final_project.Models;
 using final_project.Data;
+using final_project.Controllers;
+using System.Runtime.InteropServices;
 
 namespace final_project.Controllers
 {
@@ -15,7 +17,10 @@ namespace final_project.Controllers
         }
 
         //[HttpPost]
-        //public IActionResult Post(PizzaRestaurant)
+        public IActionResult Post(PizzaRestaurant pizzaRestaurant)
+        {
+            _context.Add(pizzaRestaurant);
+        }
 
         //[HttpPut]
         //public IActionResult Put(PizzaRestaurant pizzaRestaurant)
---
+++ Data/PizzaRestaurantDAO.cs
@@ -0,0 +1,30 @@
+﻿using final_project.Models;
+
+namespace final_project.Data
+{
+    public class PizzaRestaurantDAO
+    {
+        public TeamMemberContext _context;
+        public PizzaRestaurantDAO(TeamMemberContext context)
+        {
+        
+            _context = context;
+        }
+
+        public int? Add(PizzaRestaurant pizzaRestaurant)
+        {
+            //var restaurants = _context.PizzaRestaurants.Where(entry => entry.Id)
+
+            try
+            {
+                _context.PizzaRestaurants.Add(pizzaRestaurant);
+                _context.SaveChanges();
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+    }
+}
